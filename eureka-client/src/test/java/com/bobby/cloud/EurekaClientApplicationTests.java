c70783a23e08dc66bb6bd2d9489364cd7699916e
bobby <bobby_white@163.com>
2020-04-24 10:50:19 +0800

    基础的eureka-client 和 eureka-server

---
+++ eureka-client/src/test/java/com/bobby/cloud/EurekaClientApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bobby.cloud;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
