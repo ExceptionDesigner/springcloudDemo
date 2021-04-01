704ab3da419d4b7a33d1f4c1152259e732afb8e3
bobby <bobby_white@163.com>
2021-04-01 15:23:20 +0800

    Robin服务

---
+++ user-service/src/main/java/com/bobby/cloud/userservice/service/UserService.java
@@ -0,0 +1,24 @@
+package com.bobby.cloud.userservice.service;
+
+import com.bobby.cloud.userservice.domain.User;
+
+import java.util.List;
+
+/**
+ * @author: Bobby
+ * @create: 2020-04-24 17:58
+ * @description:
+ **/
+public interface UserService {
+    void create(User user);
+
+    User getUser(Long id);
+
+    List<User> getUserByIds(List<Long> ids);
+
+    User getByUsername(String username);
+
+    void update(User user);
+
+    void delete(Long id);
+}
